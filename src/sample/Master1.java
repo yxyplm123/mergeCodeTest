5b38475046164eae2baf83b1ec0720c658aef9ca
yanxinyu <2425453191@qq.com>
2020-12-03 23:01:03 +0800

    master bbb

--- src/sample/Master1.java
+++ src/sample/Master1.java
@@ -4,5 +4,8 @@ public class Master1 {
     public static void main(String[] args) {
        String b = "master1";
         System.out.println("aaa");
+
+        System.out.println("bbb");
+        System.out.println("ccc");
     }
 }
