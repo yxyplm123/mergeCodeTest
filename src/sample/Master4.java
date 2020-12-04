77e08cf0187902f1c9bbd76c4332323f396ae579
yanxinyu <2425453191@qq.com>
2020-12-04 15:12:05 +0800

    新的类

---
+++ src/sample/Master4.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Master4 {
+    public static void main(String[] args) {
+        System.out.println("创建新的类 分支4");
+        System.out.println("你好，北京，我知道你在哪里");
+
+    }
+}
