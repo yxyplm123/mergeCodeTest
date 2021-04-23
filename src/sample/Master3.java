be84865f6a713a7383770b9e486246e20b1bac3a
yanxinyu <2425453191@qq.com>
2021-04-23 11:26:28 +0800

    打印数字

--- src/sample/Master3.java
+++ src/sample/Master3.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 public class Master3 {
@@ -10,19 +11,22 @@ public class Master3 {
 
         String name = "是他就是他，我们的小英雄，小哪吒";
 
+        System.out.println("uuid:"+ UUID.randomUUID().toString());
         System.out.println(str+":"+name);
 
-        String person = "刘德华你在哪里";
+      int i = 6;
 
-        String city ="成都，北京，上海";
-        String eat = "麻婆豆腐";
-        String fiesty = "春节";
-        int sum = 9;
-        for(int i=0;i<sum;i++){
-            System.out.println("打印出各个数据:"+i);
-        }
-
-        String string = "nihao";
-        String name_num2 = "刘德华，郭富城，黎明，张学友";
+      switch (i){
+          case 1:
+              System.out.println("1");break;
+          case 2:
+              System.out.println("2");break;
+          case 5:
+          case 4:
+              System.out.println("5");
+            break;
+          default:
+              System.out.println("1111111");
+      }
     }
 }
