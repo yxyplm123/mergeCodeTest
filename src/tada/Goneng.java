0d7a856e7c4cb689eb1f4f65e3c384064a466882
yanxinyu <2425453191@qq.com>
2021-09-06 14:13:58 +0800

    新增查询

--- src/tada/Goneng.java
+++ src/tada/Goneng.java
@@ -4,6 +4,9 @@ public class Goneng implements HelloService{
     @Override
     public int function(String name) {
         System.out.println("写好的功能"+name);
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
         return 2;
     }
 }
